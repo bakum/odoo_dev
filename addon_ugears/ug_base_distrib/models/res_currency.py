ca249f4c24e0743b725ca8e2f59ab078e29110a1
bakum <bakum.v@gmail.com>
2023-09-19 18:07:44 +0300

    init

--- addon_ugears/ug_base_distrib/models/res_currency.py
+++ addon_ugears/ug_base_distrib/models/res_currency.py
@@ -1,4 +1,8 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+import json
+from urllib.request import urlopen
+
+from odoo.exceptions import UserError
 
 
 class ResCurrency(models.Model):
@@ -7,9 +11,19 @@ class ResCurrency(models.Model):
 
     @api.model
     def _update_val_code(self):
-        for vals in self.search([('name','=','USD')]):
+        for vals in self.search([('name', '=', 'USD')]):
             vals.code = "840"
-        for vals in self.search([('name','=','EUR')]):
+        for vals in self.search([('name', '=', 'EUR')]):
             vals.code = "978"
-        for vals in self.search([('name','=','UAH')]):
+        for vals in self.search([('name', '=', 'UAH')]):
             vals.code = "980"
+
+    def load_from_nbu(self):
+        for rec in self:
+            URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={0}&json'.format(rec.name)
+            try:
+                res = json.load(urlopen(URL))
+            except:
+                raise UserError(_('Could not connect to %s' % URL))
+            for line in res:
+                rec.code = line.get('r030', '')
