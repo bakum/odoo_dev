d3626c05808e41023e52945397ce190d304406af
bakum <bakum.v@gmail.com>
2023-10-02 17:21:30 +0300

    init

--- addon_ugears/ug_1c_connector/controllers/dictionaries.py
+++ addon_ugears/ug_1c_connector/controllers/dictionaries.py
@@ -6,10 +6,16 @@ from odoo import http
 
 
 class PublicCategoryController(http.Controller):
+    @http.route(['/api/v2/test', ],
+                auth='none', website=False, type='json', cors=False,
+                methods=['GET'])
+    def test(self):
+        return {"success": True}
+
     @http.route(['/api/v2/category',
                  '/api/v2/category/<string:guid>',
                  ],
-                auth='bearer_api_key', website=False, type='json', cors=True,
+                auth='bearer_api_key', website=False, type='json', cors=False,
                 methods=['GET', 'PUT', 'POST', 'DELETE'])
     def index(self, guid=None, **kw):
         search_criterias = get_search_criterias(kw)
