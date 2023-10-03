cc4417cdfc25f834f047044c8070b45c3b4e5ab5
bakum <bakum.v@gmail.com>
2023-10-03 14:51:16 +0300

    init

--- addon_ugears/ug_1c_connector/controllers/dictionaries.py
+++ addon_ugears/ug_1c_connector/controllers/dictionaries.py
@@ -17,15 +17,15 @@ class PublicCategoryController(http.Controller):
     @http.route(['/api/v2/category',
                  '/api/v2/category/<string:guid>',
                  ],
-                auth='bearer_api_key', website=False, type='json', cors="*", csrf=False,
+                auth='bearer_api_key', website=False, cors="*", csrf=False,
                 methods=['GET', 'PUT', 'POST', 'DELETE'])
     def index(self, guid=None, **kw):
         search_criterias = get_search_criterias(kw)
         result_dict = apply_update_from_request(kw, http.request, search_criterias, 'product.category', guid)
         if type(result_dict) is dict:
-            return result_dict
+            return json.dumps(result_dict)
         result = []
         for move in result_dict:
             mod = Category.from_orm(move).dict()
             result.append(mod)
-        return result
+        return json.dumps(result)
