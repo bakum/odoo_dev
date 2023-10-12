03bb01e97b59c52fdbefd7c1572c7b5129892e24
bakum <bakum.v@gmail.com>
2023-10-12 12:02:44 +0300

    init

---
+++ addon_ugears/ug_import_rates_from_nbu/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Import of NBU currency rates',
+    'author' : 'Bakum Viacheslav',
+    'website' : 'https://ugears.ua',
+    'category': 'Sales/Distribution Management',
+    'version': '16.0.0.1',
+    'license' : 'LGPL-3',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_currency_views.xml',
+        'wizard/import_rates_views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'ug_import_rates_from_nbu/static/src/views/res_currency_list_controller.js',
+            'ug_import_rates_from_nbu/static/src/views/res_currency_list_view.js',
+            'ug_import_rates_from_nbu/static/src/views/res_currency_list.xml',
+        ],
+    },
+    'application': True,
+}
