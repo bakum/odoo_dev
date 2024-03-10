3fd76844bb5e0a780bd3d305d7a6a0e7e603f3b6
bakum <bakum.v@gmail.com>
2024-03-10 13:32:57 +0200

    scada

--- addon_scada/nw_server/models/nw_type_of_controllers.py
+++ addon_scada/nw_server/models/nw_type_of_controllers.py
@@ -5,5 +5,5 @@ class TypesOfControllers(models.Model):
     _name = 'nw.types.controllers'
     _description = 'Types of controllers'
 
-    name = fields.Char(string='Name', required=True)
+    name = fields.Char(string='Name', required=True, default='New')
     active = fields.Boolean(default=True)
