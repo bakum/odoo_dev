c029fecb90a0c572178d120c3d575a26babbe442
bakum.v@gmail.com <bakum.v@gmail.com>
2024-03-16 13:16:28 +0200

    asa

--- addon_scada/nw_server/static/src/components/weight_dashboard.js
+++ addon_scada/nw_server/static/src/components/weight_dashboard.js
@@ -1,8 +1,8 @@
 /** @odoo-module */
 
 import { registry } from "@web/core/registry"
-// import { KpiCard } from "./kpi_card/kpi_card"
-// import { ChartRenderer } from "./chart_renderer/chart_renderer"
+import { KpiCard } from "./kpi_card/kpi_card"
+import { ChartRenderer } from "./chart_renderer/chart_renderer"
 import { loadJS } from "@web/core/assets"
 const { Component, onWillStart, useRef, onMounted } = owl
 
@@ -13,6 +13,6 @@ export class OwlWeightDashboard extends Component {
 }
 
 OwlWeightDashboard.template = "owl.OwlWeightDashboard"
-// OwlWeightDashboard.components = { KpiCard, ChartRenderer }
+OwlWeightDashboard.components = { KpiCard, ChartRenderer }
 
 registry.category("actions").add("owl.weight_dashboard", OwlWeightDashboard)
