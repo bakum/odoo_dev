e1b2bfb43a3c7151ed68517fc3f5f2eae17c376e
bakum.v@gmail.com <bakum.v@gmail.com>
2024-03-18 19:24:09 +0200

    asa

--- addon_scada/nw_server/models/nw_weight.py
+++ addon_scada/nw_server/models/nw_weight.py
@@ -12,3 +12,36 @@ class Weight(models.Model):
     value2 = fields.Float('Value2', required=True, default=0)
     error = fields.Integer(string='Error', default=0, required=True)
     message = fields.Text(string='Message')
+
+    def compute_current_weight(self, moxa_id=0):
+        sql = """SELECT moxa_id, nm.name,
+                        nm.ip,
+                         nm.port,
+                         nm.module,
+                         lval1 AS val1,
+                         lval2 AS val2,
+                         lnid AS nid,
+                         lerror AS error,
+                         lmessage AS message
+                  FROM                  (
+                          SELECT DISTINCT moxa_id,
+                                          LAST_VALUE(VALUE1) OVER (PARTITION BY moxa_id
+                                              ORDER BY write_date RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS lVal1,
+                                          LAST_VALUE(VALUE2) OVER (PARTITION BY moxa_id
+                                              ORDER BY write_date RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS lVal2,
+                                          LAST_VALUE(nid) OVER (PARTITION BY moxa_id
+                                              ORDER BY write_date RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS lnid,
+                                          LAST_VALUE(error) OVER (PARTITION BY moxa_id
+                                              ORDER BY write_date RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS lerror,
+                                          LAST_VALUE(message) OVER (PARTITION BY moxa_id
+                                              ORDER BY write_date RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS lmessage
+                          FROM nw_weight) AS weight
+                          LEFT JOIN nw_moxa nm ON nm.id = weight.moxa_id
+                  WHERE nm.active = true and moxa_id = %s
+                  """
+        self.env.cr.execute(sql, [moxa_id])
+        curr_weight = self.env.cr.dictfetchall()
+        print(moxa_id)
+        if len(curr_weight) == 0:
+            return {}
+        return curr_weight[0]
--- addon_scada/nw_server/static/src/components/weight_dashboard.js
+++ addon_scada/nw_server/static/src/components/weight_dashboard.js
@@ -5,18 +5,17 @@ import {KpiCard} from "./kpi_card/kpi_card"
 import {ChartRenderer} from "./chart_renderer/chart_renderer"
 import {loadJS} from "@web/core/assets"
 
-const {Component, onWillStart, useRef, onMounted, useState} = owl
+const {Component, onWillStart, useRef, onWillDestroy, onUnMounted, useState} = owl
 import {useService} from "@web/core/utils/hooks"
 
 export class OwlWeightDashboard extends Component {
 
-     getDates(){
+
+    getDates() {
         this.state.current_date = moment().subtract(this.state.period, 'days').format('YYYY-MM-DD')
         this.state.previous_date = moment().subtract(this.state.period * 2, 'days').format('YYYY-MM-DD')
-
-         console.log(this.state.current_date)
-         console.log(this.state.previous_date)
     }
+
     async getControllers() {
         let domain = [['active', '=', true]]
         const data = await this.orm.searchRead("nw.moxa", domain, ['id', 'name'])
@@ -27,31 +26,82 @@ export class OwlWeightDashboard extends Component {
     }
 
     async getWeight() {
-        let domain = []
+        let domain = [],
+            null_data = {
+                "id": Number(this.state.current_controller),
+                "nid" : "none",
+                "count": 0,
+                "value1": 0,
+                "value2": 0,
+            }
         if (this.state.current_controller > 0) {
-             domain.push(['moxa_id','=', this.state.current_controller])
+            domain.push(['moxa_id', '=', Number(this.state.current_controller)])
+            domain.push(['count', '>', 1])
         }
-        const data = await this.orm.searchRead("nw.weight", domain, ['id', 'nid','count','value1','value2'],{limit: 1, order:'write_date desc'})
+        if (this.state.period > 0) {
+            domain.push(['write_date', '>', this.state.current_date])
+        }
+        const data = await this.orm.searchRead("nw.weight", domain, ['id', 'nid', 'count', 'value1', 'value2'], {
+            limit: 1,
+            order: 'write_date desc'
+        })
+        const data_group = await this.orm.readGroup("nw.weight", domain, ["value1:sum", "value2:sum", "count:max"], [])
+        const data_last = await this.orm.call('nw.weight','compute_current_weight',["",this.state.current_controller],{})
 
-        console.log("weight", data)
+        this.state.weight_data = data.length > 0 ? data[0] : null_data
+        // console.log("current_controller", this.state.current_controller)
+        console.log("weight", data_last)
+        // console.log("domain", domain)
     }
 
     async getErrors() {
-        let domain = []
+        let domain = [],
+            null_data = {
+                "id": Number(this.state.current_controller),
+                "nid" : "none",
+                "count": 0,
+                "value": 0,
+            }
         if (this.state.current_controller > 0) {
-             domain.push(['moxa_id','=', this.state.current_controller])
+            domain.push(['moxa_id', '=', Number(this.state.current_controller)])
         }
-        const data = await this.orm.searchRead("nw.error", domain, ['id', 'nid','count','value'],{limit: 1, order:'write_date desc'})
+        const data = await this.orm.searchRead("nw.error", domain, ['id', 'nid', 'count', 'value'], {
+            limit: 1,
+            order: 'write_date desc'
+        })
+        this.state.error_data = data.length > 0 ? data[0] : null_data
+        // console.log("error", data)
+    }
 
-        console.log("error", data)
+    async onChangePeriod() {
+        this.getDates()
+        await this.getWeight()
+        await this.getErrors()
+    }
+
+    async onChangeController() {
+        await this.getWeight()
+        await this.getErrors()
     }
 
     setup() {
         this.state = useState({
-            period: 90,
+            period: 60,
+            current_controller: 0,
+            weight_data: {},
+            error_data: {},
+            weight: {
+                value:10,
+                percentage:6,
+            }
         })
         this.orm = useService("orm")
         this.actionService = useService("action")
+
+        this.refreshIntervalId = setInterval(async () => {
+            await this.getWeight()
+            await this.getErrors()
+        }, 5000)
 
         onWillStart(async () => {
             // await loadJS("/nw_server/static/src/components/moment/moment.min.js")
@@ -60,6 +110,10 @@ export class OwlWeightDashboard extends Component {
             await this.getWeight()
             await this.getErrors()
 
+        })
+
+        onWillDestroy(()=>{
+            clearInterval(this.refreshIntervalId)
         })
     }
 
