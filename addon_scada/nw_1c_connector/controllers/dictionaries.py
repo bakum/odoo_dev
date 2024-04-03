ad1b3a27e31db73a49778dee86687bb96eb73ac1
bakum.v@gmail.com <bakum.v@gmail.com>
2024-04-03 22:31:37 +0300

    asa

--- addon_scada/nw_1c_connector/controllers/dictionaries.py
+++ addon_scada/nw_1c_connector/controllers/dictionaries.py
@@ -12,14 +12,16 @@ class DictionariesController(http.Controller):
     def test(self):
         return json.dumps({"success": True})
 
-    @http.route(['/api/v2/<string:modelname>'],
+    @http.route(['/api/v2/<string:modelname>',
+                 '/api/v2/<string:modelname>/<string:guid>',
+                 ],
                 auth='bearer_api_key', website=False, cors="*", csrf=False,
                 methods=['GET', 'PUT', 'POST', 'DELETE'])
-    def index(self, **kw):
+    def index(self, guid=None, **kw):
         model_name = kw['modelname']
         del kw['modelname']
         data_for_edit, sk = parse_data_from_request(kw)
-        result_dict = apply_update_from_request(sk, data_for_edit, model_name)
+        result_dict = apply_update_from_request(sk, data_for_edit, model_name, guid)
         # result = http.request.env[model_name].sudo().search_read(sk)
 
         if type(result_dict) is dict:
