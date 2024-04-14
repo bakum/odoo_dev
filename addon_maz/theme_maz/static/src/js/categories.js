71dd38b799c3a5fee423ddd78f635652aa3aad0b
bakum.v@gmail.com <bakum.v@gmail.com>
2024-04-14 08:38:17 +0300

    asa

--- addon_maz/theme_maz/static/src/js/categories.js
+++ addon_maz/theme_maz/static/src/js/categories.js
@@ -14,6 +14,7 @@ export const MazCategories = PublicWidget.Widget.extend({
             }).then(data => {
                 let html = ``
                  data.forEach(category=>{
+                     if (category.image_512) {
                       html += ` <div class="col-lg-3 s_col_no_bgcolor pt16 pb16">
                                     <div class="card text-bg-white">
                                         <a href="/shop/category/${category.id}">
@@ -26,6 +27,20 @@ export const MazCategories = PublicWidget.Widget.extend({
                                         </a>
                                     </div>
                                 </div>`
+                     } else {
+                          html += ` <div class="col-lg-3 s_col_no_bgcolor pt16 pb16">
+                                    <div class="card text-bg-white">
+                                        <a href="/shop/category/${category.id}">
+                                            <h5 class="card-header tit o_default_snippet_text">${category.name}</h5>
+                                            <img class="card-img-top" src="/theme_maz/static/src/img/no.jpg"
+                                                alt="" style="width: auto; height: 165px;"/>
+                                             <div class="card-footer">
+                                                <p class="card-text cat">${category.brand}</p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>`
+                     }
                  })
                 categoriesRow.innerHTML = html
             })
