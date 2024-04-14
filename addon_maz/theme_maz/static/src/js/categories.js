1e0b2c495d10b1dbc42efc2fe212b5ddb2fa915d
bakum <bakum.v@gmail.com>
2024-04-14 07:01:41 +0300

    scada

--- addon_maz/theme_maz/static/src/js/categories.js
+++ addon_maz/theme_maz/static/src/js/categories.js
@@ -17,11 +17,11 @@ export const MazCategories = PublicWidget.Widget.extend({
                       html += ` <div class="col-lg-3 s_col_no_bgcolor pt16 pb16">
                                     <div class="card text-bg-white">
                                         <a href="/shop/category/${category.id}">
-                                            <h5 class="card-header o_default_snippet_text">${category.name}</h5>
+                                            <h5 class="card-header tit o_default_snippet_text">${category.name}</h5>
                                             <img class="card-img-top" src="data:image/*;base64,${category.image_512}"
                                                 alt="" style="width: auto; height: 165px;"/>
                                              <div class="card-footer">
-                                                <p class="card-text">${category.brand}</p>
+                                                <p class="card-text cat">${category.brand}</p>
                                             </div>
                                         </a>
                                     </div>
