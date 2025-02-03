b4306d27e34ad64d8f390f90d2b1874d6a750697
bakum <bakum.v@gmail.com>
2025-02-03 10:50:38 +0200

    spreadsheet

--- addon_ugears/ug_base_distrib/static/src/components/distrib_dashboard.js
+++ addon_ugears/ug_base_distrib/static/src/components/distrib_dashboard.js
@@ -17,18 +17,26 @@ export class OwlDistribDashboard extends Component {
 
         const old_chartjs = document.querySelector('script[src="/web/static/lib/Chart/Chart.js"]')
         const router = useService("router")
-
+        // console.log("router.current", router.current)
         if (old_chartjs) {
             let tag = 'owl.sales_dashboard';
+            let root_menu = 'menu_distrib_root';
             let model = 'ir.actions.client';
             const ids = await this.orm.searchRead(model, [['tag', '=', tag]], ['id'], {
                 limit: 1,
             })
+            const menu_ids = await this.orm.searchRead('ir.model.data', [['name', '=', root_menu]], ['res_id'], {
+                limit: 1,
+            })
+
             if (ids.length > 0) {
                 let action_id = ids[0]
+                let menu_id = menu_ids[0]
                 let {search, hash} = router.current
                 search.old_chartjs = old_chartjs != null ? "0" : "1"
                 hash.action = action_id.id
+                hash.menu_id = menu_id.res_id
+                // console.log("router.current", router.current)
                 browser.location.href = browser.location.origin + routeToUrl(router.current)
             }
         }
