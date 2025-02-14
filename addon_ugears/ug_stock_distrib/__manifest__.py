60968d6ede89d73d4025c08e8e6f5c3371304905
bakum <bakum.v@gmail.com>
2025-02-14 08:08:06 +0200

    spreadsheet

---
+++ addon_ugears/ug_stock_distrib/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Distribution''s Stock',
+    'author': 'Bakum Viacheslav',
+    'website': 'https://ugears.ua',
+    'summary': 'Yours stock for serve distribution',
+    'category': 'Sales/Distribution Management',
+    'version': '16.0.0.1',
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'depends': [
+        'ug_base_distrib',
+        'sale',
+        'stock'
+    ],
+}
--- addon_ugears/ug_wholesale_shop/report/report_distrib_quantity.py
+++ addon_ugears/ug_wholesale_shop/report/report_distrib_quantity.py
@@ -19,171 +19,76 @@ class ReportDistribQuantity(models.Model):
         tools.drop_view_if_exists(self._cr, 'report_distrib_quantity')
         query = """
         CREATE or REPLACE VIEW report_distrib_quantity AS (
-            WITH
-        DISTRIBUTOR AS (
-            SELECT
-                DIST.ID
-            FROM
-                DISTRIB_DISTRIBUTORS AS DIST
-        ),
-        EXISTING_DM (
-            ID,
-            DISTRIB_ID,
-            PRODUCT_ID,
-            TMPL_ID,
-            PRODUCT_QTY,
-            DATE,
-            STATE
-        ) AS (
-            SELECT
-                DM.ID,
-                DM.DISTRIB_ID,
-                DM.PRODUCT_ID,
-                PT.ID,
-                DM.BALANCE,
-                DM.DATE,
-                DM.STATE
-            FROM
-                DISTRIB_DISTRIBUTORS_MOVE_LINE DM
-                LEFT JOIN DISTRIBUTOR SOURCE ON SOURCE.ID = DM.DISTRIB_ID
-                LEFT JOIN PRODUCT_PRODUCT PP ON PP.ID = DM.PRODUCT_ID
-                LEFT JOIN PRODUCT_TEMPLATE PT ON PT.ID = PP.PRODUCT_TMPL_ID
-            WHERE
-                PT.TYPE in ('consu','product')
-                AND DM.PRODUCT_UOM_QTY != 0
-                AND DM.STATE NOT IN ('draft', 'cancel')
-        ),
-        QUANTS (
-            ID,
-            DISTRIB_ID,
-            PRODUCT_ID,
-            TMPL_ID,
-            PRODUCT_QTY,
-            DATE,
-            STATE
-        ) AS (
-            SELECT
-                Q.ID,
-                Q.DISTRIB_ID,
-                Q.PRODUCT_ID,
-                PP.PRODUCT_TMPL_ID,
-                Q.QUANTITY,
-                DATE.*::DATE,
-                'forecast' AS STATE
-            FROM
-                GENERATE_SERIES(
-                    (NOW() AT TIME ZONE 'utc')::DATE - INTERVAL '%(report_period)s month',
-                    (NOW() AT TIME ZONE 'utc')::DATE + INTERVAL '%(report_period)s month',
-                    '1 day'::INTERVAL
-                ) DATE,
-                DISTRIB_QUANT Q
-                LEFT JOIN PRODUCT_PRODUCT PP ON PP.ID = Q.PRODUCT_ID
-        ),
-        ALL_DM (
-            ID,
-            DISTRIB_ID,
-            PRODUCT_ID,
-            TMPL_ID,
-            PRODUCT_QTY,
-            DATE,
-            REAL_DATE,
-            STATE
-        ) AS (
-            SELECT
+        WITH
+            DISTRIBUTOR AS (
+                SELECT
+                    DIST.ID,
+                    DIST.REGION_ID
+                FROM
+                    DISTRIB_DISTRIBUTORS AS DIST
+            ),
+            QTT_REMAINS (
                 ID,
-                DISTRIB_ID,
                 PRODUCT_ID,
-                TMPL_ID,
+                DISTRIB_ID,
+                REGION_ID,
+                DATE,
+                CURRENCY_ID,
+                CATEG_ID,
+                CARTOON_ID,
+                PRICE_UNIT,
+                RATE,
+                START_PRODUCT_QTY,
                 PRODUCT_QTY,
-                GENERATE_SERIES(
-                    (NOW() AT TIME ZONE 'utc')::DATE - INTERVAL '%(report_period)s month',
-                    M.DATE::DATE + INTERVAL '%(report_period)s month',
-                    '1 day'::INTERVAL
-                )::DATE DATE,
-                M.DATE::DATE AS REAL_DATE,
-                'forecast' AS STATE
-            FROM
-                EXISTING_DM AS M
-        )
-    SELECT
-        ROW_NUMBER() OVER (
-		ORDER BY
-			MAIN.ID
-	    ) AS ID,
-        MAIN.PRODUCT_ID,
-        MAIN.STATE,
-        MAIN.DATE,
-        MAIN.DISTRIB_ID,
-        PT.CATEG_ID AS CATEG_ID,
-        PT.CARTOON_ID AS CARTOON_ID,
-        PT.QTY_IN_CARTOON AS QTY_IN_CARTOON,
-        MAIN.PRODUCT_QTY,
-        CASE
-            WHEN PT.QTY_IN_CARTOON = 0 THEN 0
-            ELSE ROUND(
-                (MAIN.PRODUCT_QTY / PT.QTY_IN_CARTOON)::NUMERIC,
-                2
+                START_PRICE_TOTAL,
+                START_PRICE_TOTAL_ACC,
+                PRICE_TOTAL,
+                PRICE_TOTAL_ACC,
+                QTY_IN_CARTOON
+            ) AS (
+                SELECT
+                    H.ID,
+                    PRODUCT_ID,
+                    DISTRIB_ID,
+                    D.REGION_ID,
+                    H.DATE::DATE AS REAL_DATE,
+                    CURRENCY_ID,
+                    PT.CATEG_ID AS CATEG_ID,
+                    PT.CARTOON_ID AS CARTOON_ID,
+                    PRICE_UNIT,
+                    ROUND(RATE::NUMERIC, 5) AS RATE,
+                    QUANTITY_BEGIN,
+                    QUANTITY_END,
+                    ROUND((PRICE_UNIT * QUANTITY_BEGIN)::NUMERIC, 2) AS START_PRICE_TOTAL,
+                    ROUND((PRICE_UNIT * QUANTITY_BEGIN * RATE)::NUMERIC, 2) AS START_PRICE_TOTAL_ACC,
+                    ROUND((PRICE_UNIT * QUANTITY_END)::NUMERIC, 2) AS PRICE_TOTAL,
+                    ROUND((PRICE_UNIT * QUANTITY_END * RATE)::NUMERIC, 2) AS PRICE_TOTAL_ACC,
+                    PT.QTY_IN_CARTOON AS QTY_IN_CARTOON
+                FROM
+                    DISTRIB_QUANT_HISTORY H
+                    LEFT JOIN PRODUCT_PRODUCT PP ON PP.ID = H.PRODUCT_ID
+                    LEFT JOIN PRODUCT_TEMPLATE PT ON PT.ID = PP.PRODUCT_TMPL_ID
+                    LEFT JOIN DISTRIBUTOR D ON D.ID = H.DISTRIB_ID
             )
-        END AS QTY_ALL_IN_CARTOON
-    FROM
-        (
-            SELECT
-                MIN(FORECAST_QTY.ID) AS ID,
-                PRODUCT_ID,
-                TMPL_ID PRODUCT_TMPL_ID,
-                STATE,
-                DATE,
-                DISTRIB_ID,
-                SUM(PRODUCT_QTY) AS PRODUCT_QTY
-            FROM
-                (
-                    SELECT
-                        M.ID,
-                        M.PRODUCT_ID,
-                        M.TMPL_ID,
-                        CASE
-                            WHEN M.PRODUCT_QTY < 0 THEN 'out'
-                            WHEN M.PRODUCT_QTY > 0 THEN 'in'
-                        END AS STATE,
-                        M.DATE::DATE AS DATE,
-                        DISTRIB_ID,
-                        PRODUCT_QTY
-                    FROM
-                        EXISTING_DM M
-                    UNION ALL
-                    SELECT
-                        ID,
-                        PRODUCT_ID,
-                        TMPL_ID,
-                        STATE,
-                        DATE,
-                        DISTRIB_ID,
-                        PRODUCT_QTY
-                    FROM
-                        QUANTS
-                    UNION ALL
-                    SELECT
-                        ID,
-                        PRODUCT_ID,
-                        TMPL_ID,
-                        STATE,
-                        DATE,
-                        DISTRIB_ID,
-                        CASE
-                            WHEN REAL_DATE > DATE THEN - PRODUCT_QTY
-                            ELSE 0
-                        END AS PRODUCT_QTY
-                    FROM
-                        ALL_DM
-                ) AS FORECAST_QTY
-            GROUP BY
-                PRODUCT_ID,
-                TMPL_ID,
-                STATE,
-                DATE,
-                DISTRIB_ID
-        ) AS MAIN
-        LEFT JOIN PRODUCT_TEMPLATE PT ON PT.ID = MAIN.PRODUCT_TMPL_ID
+        SELECT
+            ID,
+            PRODUCT_ID,
+            'forecast' STATE,
+            DATE,
+            DISTRIB_ID,
+            CATEG_ID,
+            CARTOON_ID,
+            QTY_IN_CARTOON,
+            PRODUCT_QTY,
+            CASE
+                WHEN QTY_IN_CARTOON = 0 THEN 0
+                ELSE ROUND(
+                    (PRODUCT_QTY / QTY_IN_CARTOON)::NUMERIC,
+                    2
+                )
+            END AS QTY_ALL_IN_CARTOON
+        FROM
+            QTT_REMAINS
     );
     """
         report_period = self.env['ir.config_parameter'].sudo().get_param('distrib.report_distrib_quantity_period',
