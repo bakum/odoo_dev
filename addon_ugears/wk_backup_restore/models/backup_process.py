e4e85ceb70ecb8f84fe2158e850f037b8ac5cae7
bakum <bakum.v@gmail.com>
2025-02-26 21:17:08 +0200

    update backup_process.py

--- addon_ugears/wk_backup_restore/models/backup_process.py
+++ addon_ugears/wk_backup_restore/models/backup_process.py
@@ -12,7 +12,7 @@ from odoo.exceptions import UserError
 from odoo.modules.module import get_module_resource
 from odoo.tools.config import config
 
-from models.lib import manage_backup_crons, saas_client_backup
+from lib import manage_backup_crons, saas_client_backup
 from datetime import datetime
 import subprocess
 import os
