afb90e25c0e6ec06727b250d01009cf6e7096036
bakum <bakum.v@gmail.com>
2025-03-15 10:38:23 +0200

    spreadsheet

---
+++ addon_ugears/complete_data_clear_and_removal/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Complete Data Clear And Removal",
+    "version": "16.0.1.0.0",
+    "summary": "This app enables efficient bulk data clearing and deletion, providing a quick solution for comprehensive data cleanup and management.",
+    "category": "Deletion Tool",
+    "license": "LGPL-3",
+    "author": "Bakum Viacheslav",
+    "website": "https://ugears.ua",
+    "description": "This app is designed to simplify large-scale data management, offering tools to clear and delete, data in bulk.",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/qcent_mass_clear_and_delete_data_menu.xml",
+        "wizard/clear_all_data.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+    "installable": True,
+}
