e9a02b802f8ac6a7413ace2832c26ff21ba4b209
bakum <bakum.v@gmail.com>
2025-03-20 10:18:49 +0200

    spreadsheet

---
+++ addon_ugears/ug_website_inhertance_ext/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Extended website inheritance",
+    "version": "16.0.0.1",
+    'author': 'Bakum Viacheslav',
+    'website': 'https://ugears.ua',
+    "license": "LGPL-3",
+    "category": "Hidden/Dependency",
+    "summary": "Adds more operators for view inheritance",
+    "depends": ["base", "website"],
+}
