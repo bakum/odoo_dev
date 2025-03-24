6cb53bf3dcc84f145a74a6e70b0393eb9b77eefa
bakum <bakum.v@gmail.com>
2025-03-24 19:45:25 +0200

    spreadsheet

--- addon_ugears/ug_base_distrib/models/res_users.py
+++ addon_ugears/ug_base_distrib/models/res_users.py
@@ -5,8 +5,10 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+
 def now(**kwargs):
     return datetime.now() + timedelta(**kwargs)
+
 
 class UsersDistrib(models.Model):
     _inherit = "res.users"
@@ -19,23 +21,14 @@ class UsersDistrib(models.Model):
             return
         if self.filtered(lambda user: not user.active):
             raise UserError(_("You cannot perform this action on an archived user."))
-        # prepare reset password signup
+
         create_mode = bool(self.env.context.get('create_user'))
-
-        # no time limit for initial invitation, only for reset password
         expiration = False if create_mode else now(days=+1)
-
         self.mapped('partner_id').signup_prepare(signup_type="reset", expiration=expiration)
 
-        # send email to users with their signup url
-        template = False
-        if create_mode:
-            try:
-                template = self.env.ref('ug_base_distrib.set_password_email', raise_if_not_found=False)
-            except ValueError:
-                pass
-        if not template:
-            template = self.env.ref('auth_signup.reset_password_email')
+        template = self.env.ref('ug_base_distrib.set_password_email',
+                                raise_if_not_found=False) if create_mode else self.env.ref(
+            'auth_signup.reset_password_email')
         assert template._name == 'mail.template'
 
         email_values = {
@@ -52,6 +45,6 @@ class UsersDistrib(models.Model):
                 raise UserError(_("Cannot send email: user %s has no email address.", user.name))
             email_values['email_to'] = user.email
             with self.env.cr.savepoint():
-                force_send = not (self.env.context.get('import_file', False))
+                force_send = not self.env.context.get('import_file', False)
                 template.send_mail(user.id, force_send=force_send, raise_exception=True, email_values=email_values)
             _logger.info("Password reset email sent for user <%s> to <%s>", user.login, user.email)
