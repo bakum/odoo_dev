1b2d36c5dfdfe96760dced061a38f2e87a29e419
bakum <bakum.v@gmail.com>
2025-04-02 09:30:56 +0300

    add translation files and manifest for extended auth_signup features

---
+++ addon_ugears/ug_auth_signup_inheritance_ext/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Extended auth_signup distribution inheritance",
+    "version": "16.0.0.1",
+    'author': 'Bakum Viacheslav',
+    'website': 'https://ugears.ua',
+    "license": "LGPL-3",
+    "category": "Hidden/Dependency",
+    "summary": "Adds more translation operators for view inheritance",
+    "depends": ["base", "auth_signup"],
+}
