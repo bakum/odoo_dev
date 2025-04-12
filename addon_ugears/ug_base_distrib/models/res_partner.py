6872756ab6148bb3969f9ca647f6058a574b3494
bakum <bakum.v@gmail.com>
2025-04-12 11:07:42 +0300

    add chatter position feature to user preferences and translations

--- addon_ugears/ug_base_distrib/models/res_partner.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, models, _, fields
-
-
-class PartnersDistrib(models.Model):
-    _inherit = "res.partner"
-
-    guid = fields.Char(string='Guid 1C:Enterprise')
--- addon_ugears/ug_base_distrib/models/res_users.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, models, _, fields
-
-
-class UsersDistrib(models.Model):
-    _inherit = "res.users"
-
-    distrib_id = fields.Many2one('distrib.distributors', "Distributor")
