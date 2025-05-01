0818381d4fe139a68eee6544db735344f41bf65d
bakum <bakum.v@gmail.com>
2025-05-01 18:34:51 +0300

    refactor: enhance API integration by adding new modules and updating manifest for Swagger UI

--- addon_ugears/ug_1c_connector/__manifest__.py
+++ addon_ugears/ug_1c_connector/__manifest__.py
@@ -11,6 +11,25 @@
         'product',
         'ug_base_distrib',
     ],
+    "data": [
+        # Security
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        # Views
+        'views/api_rest_version_views.xml',
+        'views/api_rest_path_views.xml',
+        'views/api_rest_tag_views.xml',
+        'views/api_rest_log_views.xml',
+        'views/swagger_templates.xml',
+    ],
+    'assets': {
+        'ug_1c_connector.assets_swagger': [
+            'ug_1c_connector/static/lib/swagger-ui-3.38.0/swagger-ui.css',
+            'ug_1c_connector/static/lib/swagger-ui-3.38.0/swagger-ui-bundle.js',
+            'ug_1c_connector/static/lib/swagger-ui-3.38.0/swagger-ui-standalone-preset.js',
+        ],
+    },
+
     # "external_dependencies": {
     #     "python": ["pydantic"]
     # },
