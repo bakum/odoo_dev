6bdf91194948c2de0aebb2dd593c6b5cba883f79
bakum <bakum.v@gmail.com>
2025-06-04 12:12:03 +0300

    refactor: enhance country and product name translations in horizont_xls.py

--- addon_ugears/ug_base_distrib/reports/horizont_xls.py
+++ addon_ugears/ug_base_distrib/reports/horizont_xls.py
@@ -108,9 +108,19 @@ class OrderToXlsx(models.AbstractModel):
                 row_n = 9 + count + 1
                 sheet.write(row_n, 0, count + 1, txt)
                 sheet.write(row_n, 1, '' if not line.product_id.customscode else line.product_id.customscode, txt)
-                sheet.write(row_n, 2, '' if not obj.company_id.country_id.display_name else obj.company_id.country_id.display_name, txt)
+                field = obj.company_id.country_id._fields
+                translations = field['name']._get_stored_translations(obj.company_id.country_id)
+                if translations and 'en_US' in translations:
+                    sheet.write(row_n, 2, translations['en_US'], txt)
+                else:
+                    sheet.write(row_n, 2, '' if not obj.company_id.country_id.display_name else obj.company_id.country_id.display_name, txt)
                 sheet.write(row_n, 3, '' if not line.product_id.barcode else line.product_id.barcode, txt)
-                sheet.write(row_n, 4, line.product_id.display_name, txt)
+                field = line.product_id.product_tmpl_id._fields
+                translations = field['name']._get_stored_translations(line.product_id.product_tmpl_id)
+                if translations and 'en_US' in translations:
+                    sheet.write(row_n, 4, translations['en_US'], txt)
+                else:
+                    sheet.write(row_n, 4, line.product_id.display_name, txt)
                 sheet.write(row_n, 5, line.product_uom_qty, txt)
                 all_qtt += line.product_uom_qty
                 sheet.write(row_n, 6, 'pcs', txt)
